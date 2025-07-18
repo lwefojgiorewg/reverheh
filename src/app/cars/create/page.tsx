5e44f37a30c12e8a824ae6128222a9acebb5c2b1
reversable <lovewadhwa2000@gmail.com>
2025-07-18 15:26:30 +0530

    Add default export to create car page

--- src/app/cars/create/page.tsx
+++ src/app/cars/create/page.tsx
@@ -1,1 +1,9 @@
+export default function CreateCarPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Create Car Page</h1>
+      <p>This is a placeholder for creating a car entry.</p>
+    </div>
+  );
+}
  
